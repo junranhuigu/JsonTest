5d95c338d7a35a1db66f6ab9318ad96d6f6082ef
junranhuigu <398192117@qq.com>
2017-05-25 14:30:04 +0800

    json信息提取 + 转化为数据封装类

--- src/com/json/Analysis.java
+++ src/com/json/Analysis.java
@@ -6,10 +6,6 @@ import java.util.regex.Pattern;
 
 
 class Analysis {
-	public static void main(String[] args) throws Exception{
-		String json = "{\"advantureExp\":0,\"advantureInfo\":{\"advantureCount\":0,\"advantureFreeDeployCount\":0,\"refreshTime\":1477633389173},\"advantureLevel\":1,\"advantureStageId\":0,\"battleInfo\":{\"battlecd\":0,\"enemy\":[{\"attacked\":0,\"id\":\"10092\",\"name\":\"漂泊的卡尔\",\"vipLevel\":0},{\"attacked\":0,\"id\":\"10071\",\"name\":\"树上的赫特\",\"vipLevel\":0},{\"attacked\":0,\"id\":\"10085\",\"name\":\"勇敢的加登\",\"vipLevel\":0},{\"attacked\":0,\"id\":\"10001\",\"name\":\"可爱的阿瑟\",\"vipLevel\":0},{\"attacked\":0,\"id\":\"10043\",\"name\":\"消失的杜威\",\"vipLevel\":0}],\"id\":\"1025103053777301\",\"lastAttackTime\":0,\"refreshTime\":1477362666458,\"remainRefreshCount\":5},\"defenceRecords\":[],\"id\":\"1025103053777301\",\"lastProductCalTime\":1477362666456,\"openProductTeamPosition\":1,\"productTeam\":[{\"beginProductTime\":0,\"id\":1,\"members\":[],\"state\":-1,\"typeId\":0}],\"stages\":[{\"backpack\":[],\"deployCount\":0,\"process\":0,\"stageId\":1,\"typeCounts\":{}}]}";
-		analysis(json);
-	}
 	
 	// json解析器逻辑：由于 JSON 语法特别简单 我们不需要写分词器（tokenizer） 只需检测下一个字符 便可以知道它是哪种类型的值 然后调用相关的分析函数
 	//	n ➔ null
@@ -19,7 +15,7 @@ class Analysis {
 	//	0-9/- ➔ number
 	//	[ ➔ array
 	//	{ ➔ object
-	public static void analysis(String content) throws Exception{
+	public static Value analysis(String content) throws Exception{
 		State state = new State();
 		char c = 0;
 		try {
@@ -52,8 +48,6 @@ class Analysis {
 					}
 					state.subState(State.OBJECT);
 					break;
-//			case '"':
-//				break;
 				case ':':
 					state.subState(State.MEM_KEY);
 					state.addState(State.MEM_VALUE);
@@ -79,20 +73,27 @@ class Analysis {
 							//获取字符串
 							String k = analysisString(content, i);
 							i += k.length() + 1;//还差一个"号
-							System.out.println("String " + k + ", state : " + state.stateName());
+//							System.out.println("String " + k + ", state : " + state.stateName());
+							state.addStateContent(k);
 						} else {
 							String v = analysisOthers(content, i);
 							i += v.length() - 1;//已经读了一个数字
-							System.out.println("Other " + v + ", state : " + state.stateName());
+//							System.out.println("Other " + v + ", state : " + state.stateName());
+							state.addStateContent(v);
 						}
-					} 
+					}
 					break;
 				}
-//				System.out.println("nowChar : " + c + ", " + state);
 			}
 		} finally {
-			System.out.println("nowChar : " + c + ", " + state);
+//			System.out.println("nowChar : " + c + ", " + state);
 		}
+//		System.out.println(state.getValue());
+		Value value = new Value();
+		value.generateValue(state.getValue());
+//		System.out.println(value);
+//		System.out.println(value.rootJsonString());
+		return value;
 	}
 	
 	private static String analysisOthers(String content, int index) throws Exception {
@@ -129,16 +130,6 @@ class Analysis {
 	}
 
 	private static String analysisString(String content, int index) {
-//		for(int i = index; i < content.length(); ++ i){
-//			if(content.charAt(i) == first_c){
-//				++ index;
-//			} else if(content.charAt(i) == last_c){
-//				-- index;
-//			}
-//			if(index == 0){
-//				return content.substring(index, i);
-//			}
-//		}
 		for(int i = index + 1; i < content.length(); ++ i){
 			char c = content.charAt(i);
 			if(c == '"' && content.charAt(i - 1) != '\\'){
--- src/com/json/State.java
+++ src/com/json/State.java
@@ -9,19 +9,20 @@ import java.util.Stack;
 class State {
 	private Stack<Integer> stack;
 	private int nowState;
+	private StateValue value;
 	
 	public static final int OBJECT = 1;
 	public static final int ARRAY = 2;
+	public static final int OBJECT_FINISH = 3;
+	public static final int ARRAY_FINISH = 4;
 	public static final int MEM_KEY = 5;
 	public static final int MEM_VALUE = 7;
 	public static final int ELEMENT = 8;
-//	public static final int WAIT_FEN = 6;
-//	public static final int MEM_OVER = 9;
-//	public static final int ELEMENT_OVER = 10;
 	
 	public State() {
 		this.stack = new Stack<>();
 		this.nowState = 0;
+		this.value = new StateValue();
 	}
 	
 	public void addState(int key){
@@ -31,10 +32,28 @@ class State {
 			stack.push(nowState);
 			nowState = key;
 		}
+		this.value.add(nowState, stateName());
+	}
+	
+	/**
+	 * 补全内容
+	 * */
+	public void addStateContent(String value){
+		if(state() == State.ELEMENT || state() == State.MEM_VALUE || state() == State.MEM_KEY){
+			this.value.addContent(value);
+		}
 	}
 	
 	public void subState(int key) throws Exception{
 		if(nowState == key){
+			switch (nowState) {
+			case OBJECT:
+				this.value.add(OBJECT_FINISH, name(OBJECT_FINISH));
+				break;
+			case ARRAY:
+				this.value.add(ARRAY_FINISH, name(ARRAY_FINISH));
+				break;
+			}
 			if(!stack.isEmpty()){
 				nowState = stack.pop();
 			} else {
@@ -53,11 +72,11 @@ class State {
 		return name(nowState);
 	}
 	
-	private String name(int key){
-		for(Field field : this.getClass().getFields()){
+	public static String name(int key){
+		for(Field field : State.class.getFields()){
 			if(!field.isSynthetic()){
 				try {
-					int k = field.getInt(this);
+					int k = field.getInt(null);
 					if(k == key){
 						return field.getName();
 					}
@@ -67,6 +86,10 @@ class State {
 		return null;
 	}
 	
+	public StateValue getValue() {
+		return value;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder stackInfo = new StringBuilder("");
